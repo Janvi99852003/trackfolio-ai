b6a9ad08df47d5e6be1c9a16264955010e5ce142
Janvi Jaiswal <janvijaiswal99@gmail.com>
2026-07-04 10:48:39 +0530

    Force IPv4 for Gmail SMTP to fix Render connectivity

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,11 +1,14 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD,
   },
+  family: 4, // force IPv4, avoids ENETUNREACH on some hosts
 });
 
 async function sendEmail({ to, subject, html }) {
